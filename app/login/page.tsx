abfae222a7df728e813f9c3a6c0291061085a40b
Nel <nel.renaudin@gmail.com>
2025-02-12 21:35:53 +0100

    Add links in login and register pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
 import LoginForm from "../ui/login-form"
 
 export default function LoginPage() {
   return (
     <main>
       <LoginForm />
+      <p>
+        You don't have an account yet? <Link href="/register">Register</Link>
+      </p>
     </main>
   )
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
 import RegisterForm from "../ui/register-form"
 
 export default function RegisterPage() {
   return (
     <main>
       <RegisterForm />
+      <p>
+        You already have an account? <Link href="/login">Login</Link>
+      </p>
     </main>
   )
 }
