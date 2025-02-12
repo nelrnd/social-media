88f9d1a5f65429a25b7908d782b5d355a0cfed19
Nel <nel.renaudin@gmail.com>
2025-02-12 21:25:18 +0100

    Implement server action in login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,6 +1,19 @@
+"use client"
+
+import { useActionState } from "react"
+import { authenticate } from "@/app/lib/actions"
+import { useSearchParams } from "next/navigation"
+
 export default function LoginForm() {
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get("callbackUrl") || "/"
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  )
+
   return (
-    <form>
+    <form action={formAction}>
       <h1>Log in</h1>
       <div>
         <label htmlFor="email">Email</label>
@@ -10,7 +23,11 @@ export default function LoginForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </div>
-      <button>Log in</button>
+      <input type="hidden" name="redirectTo" value={callbackUrl} />
+      <button aria-disabled={isPending}>Log in</button>
+      <div aria-live="polite" aria-atomic="true">
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </div>
     </form>
   )
 }
