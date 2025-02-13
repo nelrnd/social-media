060c3ba04c162d6ea2066c404e7202aca312bda8
Nel <nel.renaudin@gmail.com>
2025-02-13 11:26:40 +0100

    Create authorized function

--- auth.config.ts
+++ auth.config.ts
@@ -6,6 +6,16 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnProtectedRoute = !["/login", "/register"].includes(
+        nextUrl.pathname.split("?")[0]
+      )
+      if (isOnProtectedRoute) {
+        if (isLoggedIn) return true
+        return false
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl))
+      }
       return true
     },
   },
