9503e6cbf2c5b9f384981cb73f439e4561ab6e03
Nel <nel.renaudin@gmail.com>
2025-02-14 15:44:23 +0100

    Include user id in session data

--- auth.config.ts
+++ auth.config.ts
@@ -18,6 +18,18 @@ export const authConfig = {
       }
       return true
     },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
   },
   providers: [],
 } satisfies NextAuthConfig
