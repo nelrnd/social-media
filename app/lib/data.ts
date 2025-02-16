69200d06ed779479241ab5a362b0b387f165efc1
Nel <nel.renaudin@gmail.com>
2025-02-16 20:09:09 +0100

    Display post by recency order

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,6 +5,7 @@ import { prisma } from "./prisma"
 export async function fetchPosts() {
   const posts = await prisma.post.findMany({
     include: { user: { select: { profile: true } } },
+    orderBy: { createdAt: "desc" },
   })
   return posts
 }
