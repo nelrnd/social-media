9b055fc8cb80401d977256854e63fe9ac122b1cc
Nel <nel.renaudin@gmail.com>
2025-02-16 20:03:01 +0100

    Display user info on post

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,6 +3,8 @@
 import { prisma } from "./prisma"
 
 export async function fetchPosts() {
-  const posts = await prisma.post.findMany({ include: { user: true } })
+  const posts = await prisma.post.findMany({
+    include: { user: { select: { profile: true } } },
+  })
   return posts
 }
--- app/ui/post-card.tsx
+++ app/ui/post-card.tsx
@@ -1,6 +1,12 @@
 export default function PostCard({ post }) {
   return (
     <article className="p-4 border-b border-gray-200 space-y-2">
+      <header>
+        <p>
+          <span className="font-bold">{post.user.profile.name}</span>{" "}
+          {post.user.profile.username}
+        </p>
+      </header>
       <p>{post.content}</p>
     </article>
   )
