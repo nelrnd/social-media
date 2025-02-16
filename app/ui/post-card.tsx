1833e335cbdae545c94d3117de79eaf7cb4a7846
Nel <nel.renaudin@gmail.com>
2025-02-16 20:08:10 +0100

    Display date in post

--- app/ui/post-card.tsx
+++ app/ui/post-card.tsx
@@ -1,11 +1,23 @@
-export default function PostCard({ post }) {
+import { Post, Prisma } from "@prisma/client"
+import moment from "moment"
+
+export default function PostCard({
+  post,
+}: {
+  post: Prisma.PostGetPayload<{
+    include: { user: { select: { profile: true } } }
+  }>
+}) {
   return (
     <article className="p-4 border-b border-gray-200 space-y-2">
-      <header>
+      <header className="flex items-center justify-between">
         <p>
-          <span className="font-bold">{post.user.profile.name}</span>{" "}
-          {post.user.profile.username}
+          <span className="font-bold">{post.user.profile?.name}</span>{" "}
+          <span className="text-gray-600">{post.user.profile?.username}</span>
         </p>
+        <time className="text-gray-600 text-sm">
+          {moment(post.createdAt).format("LT")}
+        </time>
       </header>
       <p>{post.content}</p>
     </article>
