deb674ca6851d98fc11b6416399e83b56855e166
Nel <nel.renaudin@gmail.com>
2025-02-20 13:17:45 +0100

    Style SideBar links

--- app/ui/side-bar.tsx
+++ app/ui/side-bar.tsx
@@ -1,35 +1,73 @@
+import React from "react"
 import Link from "next/link"
 import { signOut } from "@/auth"
+import {
+  ArrowLeftStartOnRectangleIcon,
+  BellIcon,
+  Cog6ToothIcon,
+  HomeIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline"
+
+const links = [
+  {
+    href: "/",
+    text: "Home",
+    icon: <HomeIcon />,
+  },
+  {
+    href: "/notifications",
+    text: "Notifications",
+    icon: <BellIcon />,
+  },
+  {
+    href: "/profile",
+    text: "Profile",
+    icon: <UserIcon />,
+  },
+  {
+    href: "/settings",
+    text: "Settings",
+    icon: <Cog6ToothIcon />,
+  },
+]
 
 export default function SideBar() {
   return (
     <nav className="lg:w-[18rem] h-screen p-8 border-r bg-white border-gray-200 lg:fixed w-fit">
-      <ul className="flex flex-col h-full gap-4">
+      <ul className="flex flex-col gap-3 h-full">
         <li className="mb-8">
           <Link href="/">
-            <span className="text-2xl font-bold">AHSI</span>
+            <span className="text-2xl font-bold hover:underline">AHSI</span>
           </Link>
         </li>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/notifications">Notifications</Link>
-        </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link href="/settings">Settings</Link>
-        </li>
-        <li className="mt-auto">
+
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className="p-3 -mx-3 hover:bg-gray-50 rounded-full font-semibold flex items-center gap-2"
+            >
+              {React.cloneElement(link.icon, { className: "size-6" })}
+              {link.text}
+            </Link>
+          </li>
+        ))}
+
+        <li className="mt-auto -m-3">
           <form
             action={async () => {
               "use server"
               await signOut({ redirectTo: "/login" })
             }}
           >
-            <button>Log out</button>
+            <button
+              type="submit"
+              className="w-full p-3 hover:bg-red-50 rounded-full font-semibold flex items-center gap-2 text-red-500"
+            >
+              <ArrowLeftStartOnRectangleIcon className="size-6" />
+              Log out
+            </button>
           </form>
         </li>
       </ul>
