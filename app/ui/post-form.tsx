e1c71b7aa5384562dd09084b4fb9318f96b5368e
Nel <nel.renaudin@gmail.com>
2025-03-14 00:37:00 +0100

    Style post form

--- app/ui/post-form.tsx
+++ app/ui/post-form.tsx
@@ -2,6 +2,7 @@
 
 import { useActionState } from "react"
 import { createPost } from "@/app/lib/actions"
+import { LoaderCircleIcon } from "lucide-react"
 
 export default function PostForm() {
   const [formError, formAction, isPending] = useActionState(
@@ -12,19 +13,25 @@ export default function PostForm() {
   return (
     <form
       action={formAction}
-      className="p-4 border-b border-gray-200 space-y-2"
+      className="p-6 border-b border-gray-200 space-y-2"
     >
       <textarea
         placeholder="What do you want to say?"
-        className="w-full h-[4rem] p-2"
+        className="w-full h-[4rem] outline-none"
         name="content"
       ></textarea>
       {formError && <p className="text-red-500">{formError}</p>}
       <button
-        className="block w-fit ml-auto py-2 px-6 bg-gray-900 text-white"
-        aria-disabled={isPending}
+        className="w-fit h-[3.125rem] py-3 px-6 flex items-center justify-center bg-gray-900 text-white disabled:opacity-50 hover:opacity-95 transition-opacity rounded-sm ml-auto relative"
+        disabled={isPending}
       >
-        Post
+        <span className={isPending ? "invisible" : ""}>Post</span>
+        {isPending && (
+          <LoaderCircleIcon
+            className="size-4 animate-spin absolute object-center"
+            aria-label="Loading"
+          />
+        )}
       </button>
     </form>
   )
