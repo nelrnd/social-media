fccd992a95897f33118a95b7e5927448fe74d1fa
Nel <nel.renaudin@gmail.com>
2025-03-17 00:46:17 +0100

    Update profile page

--- app/(protected)/profile/[username]/page.tsx
+++ app/(protected)/profile/[username]/page.tsx
@@ -24,25 +24,29 @@ export default async function ProfilePage({
   return (
     <main>
       <header className="p-4 space-y-4 border-b border-gray-200">
-        <Avatar src={profile.imageUrl} size="xl" />
-        <div className="grid grid-cols-2 items-center">
-          <div>
+        <div className="flex items-center gap-6">
+          <Avatar src={profile.imageUrl} size="xl" />
+
+          <div className="flex-1">
             <h1 className="text-4xl font-bold">{profile?.name}</h1>
             <p className="text-gray-600">{profile?.username}</p>
           </div>
-          {sameUser ? (
-            <Link
-              href="/settings/profile"
-              className="block w-fit ml-auto py-2 px-6 border border-gray-200 disabled:opacity-50 hover:bg-gray-100 transition-colors"
-            >
-              Edit
-            </Link>
-          ) : (
-            <FollowButton
-              isFollowing={isFollowing}
-              profileId={profile?.id || "" /* to be modified */}
-            />
-          )}
+
+          <div className="w-fit">
+            {sameUser ? (
+              <Link
+                href="/settings/profile"
+                className="block w-fit ml-auto py-2 px-6 border border-gray-200 disabled:opacity-50 hover:bg-gray-100 transition-colors"
+              >
+                Edit
+              </Link>
+            ) : (
+              <FollowButton
+                isFollowing={isFollowing}
+                profileId={profile?.id || "" /* to be modified */}
+              />
+            )}
+          </div>
         </div>
         <p>{profile?.bio}</p>
         <FollowBar profile={profile} />
--- app/ui/post-form.tsx
+++ app/ui/post-form.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-import { useActionState } from "react"
+import { useActionState, useState } from "react"
 import { createPost } from "@/app/lib/actions"
 import { LoaderCircleIcon } from "lucide-react"
+import { PhotoIcon } from "@heroicons/react/24/outline"
+import Image from "next/image"
+import clsx from "clsx"
 
 export default function PostForm() {
   const [formError, formAction, isPending] = useActionState(
     createPost,
     undefined
   )
+
+  const [images, setImages] = useState<FileList | null>(null)
 
   return (
     <form
@@ -20,19 +25,70 @@ export default function PostForm() {
         className="w-full h-[4rem] outline-none"
         name="content"
       ></textarea>
+
       {formError && <p className="text-red-500">{formError}</p>}
-      <button
-        className="w-fit h-[3.125rem] py-3 px-6 flex items-center justify-center bg-gray-900 text-white disabled:opacity-50 hover:opacity-95 transition-opacity rounded-sm ml-auto relative"
-        disabled={isPending}
-      >
-        <span className={isPending ? "invisible" : ""}>Post</span>
-        {isPending && (
-          <LoaderCircleIcon
-            className="size-4 animate-spin absolute object-center"
-            aria-label="Loading"
+
+      {images && images.length && (
+        <div
+          className={clsx("grid gap-2", {
+            "grid-cols-1": images.length === 1,
+            "grid-cols-2": images.length > 1,
+          })}
+        >
+          {Array.from(images)
+            .map((image) => URL.createObjectURL(image))
+            .map((imageUrl) => (
+              <ImagePreview key={imageUrl} src={imageUrl} />
+            ))}
+        </div>
+      )}
+
+      <div className="flex items-center justify-between">
+        <div>
+          <input
+            type="file"
+            accept="image/*"
+            multiple={true}
+            onChange={(e) => setImages(e.target.files)}
+            className="hidden"
+            name="image"
+            id="image"
           />
-        )}
-      </button>
+          <label
+            htmlFor="image"
+            className="cursor-pointer size-12 bg-white hover:bg-gray-100 flex items-center justify-center rounded-full transition-all"
+          >
+            <PhotoIcon className="size-5 text-gray-600" />
+          </label>
+        </div>
+
+        <button
+          className="w-fit h-[3.125rem] py-3 px-6 flex items-center justify-center bg-gray-900 text-white disabled:opacity-50 hover:opacity-95 transition-opacity rounded-sm relative"
+          disabled={isPending}
+        >
+          <span className={isPending ? "invisible" : ""}>Post</span>
+          {isPending && (
+            <LoaderCircleIcon
+              className="size-4 animate-spin absolute object-center"
+              aria-label="Loading"
+            />
+          )}
+        </button>
+      </div>
     </form>
+  )
+}
+
+function ImagePreview({ src }: { src: string }) {
+  return (
+    <div className="w-full aspect-square relative border border-gray-100 rounded-xl overflow-hidden">
+      <Image
+        src={src}
+        alt=""
+        layout="fill"
+        objectFit="contain"
+        className="absolute inset-0"
+      />
+    </div>
   )
 }
