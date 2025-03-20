a01505c395b3ae549fc39bd8544f5f93a43d870d
Nel <nel.renaudin@gmail.com>
2025-03-20 15:00:11 +0100

    Update Notification model

---
+++ prisma/migrations/20250320135436_add_post_and_comment_field_in_notification_model/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Notification" ADD COLUMN     "commentId" TEXT,
+ADD COLUMN     "postId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
