81eeaed9b1f5a6bd3ed885fcb6e73c6a3f513391
Nel <nel.renaudin@gmail.com>
2025-03-24 18:47:58 +0100

    Add close button to image gallery

--- app/ui/image-gallery.tsx
+++ app/ui/image-gallery.tsx
@@ -8,6 +8,7 @@ import {
   DialogTitle,
 } from "@/app/ui/dialog"
 import Image from "next/image"
+import { XMarkIcon } from "@heroicons/react/24/outline"
 
 const ImageGalleryContext = createContext({
   text: "yes",
@@ -44,11 +45,19 @@ export default function ImageGalleryProvider({
       {children}
 
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="h-full max-w-full sm:rounded-none bg-black/80 border-black flex flex-col">
+        <DialogContent className="h-full max-w-full sm:rounded-none bg-black/70 border-black flex flex-col">
           <DialogTitle className="sr-only">Image gallery</DialogTitle>
           <DialogDescription className="sr-only">
             Browse through gallery
           </DialogDescription>
+
+          <button
+            onClick={closeGallery}
+            className="fixed z-20 top-8 left-8 cursor-pointer size-12 bg-black hover:bg-gray-950 border border-gray-950 flex items-center justify-center rounded-full transition-colors"
+          >
+            <span className="sr-only">Close image</span>
+            <XMarkIcon className="size-5 text-white" />
+          </button>
 
           <main className="flex-1 p-8 flex justify-center items-center relative">
             {currentImageUrl && (
