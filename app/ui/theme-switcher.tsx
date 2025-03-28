d6796d0bcee3440c38d74f6e22041fb755a45b0b
Nel <nel.renaudin@gmail.com>
2025-03-28 17:09:43 +0100

    Update ThemeSwitcher component

--- app/ui/theme-switcher.tsx
+++ app/ui/theme-switcher.tsx
@@ -1,5 +1,10 @@
 "use client"
 
+import {
+  ComputerDesktopIcon,
+  MoonIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline"
 import clsx from "clsx"
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
@@ -19,32 +24,44 @@ export default function ThemeSwitcher() {
   return (
     <div>
       <h3 className="mb-2">Theme</h3>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid min-[420px]:grid-cols-3 gap-3">
         <button
           onClick={() => setTheme("light")}
-          className={clsx("p-2 border border-border rounded-sm", {
-            "ring-2 ring-offset-2  ring-foreground ring-offset-background":
-              theme === "light",
-          })}
+          className={clsx(
+            "py-2 px-3 border border-border rounded-sm flex items-center gap-2",
+            {
+              "ring-2 ring-offset-2  ring-foreground ring-offset-background":
+                theme === "light",
+            }
+          )}
         >
+          <SunIcon className="size-5" />
           Light
         </button>
         <button
           onClick={() => setTheme("dark")}
-          className={clsx("p-2 border border-border rounded-sm", {
-            "ring-2 ring-offset-2 ring-foreground ring-offset-background":
-              theme === "dark",
-          })}
+          className={clsx(
+            "py-2 px-3 border border-border rounded-sm flex items-center gap-2",
+            {
+              "ring-2 ring-offset-2 ring-foreground ring-offset-background":
+                theme === "dark",
+            }
+          )}
         >
+          <MoonIcon className="size-5" />
           Dark
         </button>
         <button
           onClick={() => setTheme("system")}
-          className={clsx("p-2 border border-border rounded-sm", {
-            "ring-2 ring-offset-2 ring-foreground ring-offset-background":
-              theme === "system",
-          })}
+          className={clsx(
+            "py-2 px-3 border border-border rounded-sm flex items-center gap-2",
+            {
+              "ring-2 ring-offset-2 ring-foreground ring-offset-background":
+                theme === "system",
+            }
+          )}
         >
+          <ComputerDesktopIcon className="size-5" />
           System
         </button>
       </div>
