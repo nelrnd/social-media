200e6b7e0eec9984c1574ce0b6435d399370e6dc
Nel <nel.renaudin@gmail.com>
2025-03-29 11:37:23 +0100

    Add press effect in ImagePreview

--- app/ui/image-preview.tsx
+++ app/ui/image-preview.tsx
@@ -11,13 +11,32 @@ export default function ImagePreview({
   handleDelete?: () => void
   handleClick?: () => void
 }) {
-  return (
-    <div
-      className={clsx(
-        "w-full aspect-square relative border border-border rounded-xl overflow-hidden transition-all",
-        { "z-10 hover:brightness-90": !!handleClick }
-      )}
+  return handleClick ? (
+    <button
+      onClick={handleClick}
+      className="press w-full aspect-square relative border border-border rounded-xl overflow-hidden transition-all z-10 hover:brightness-90"
     >
+      {handleDelete && (
+        <button
+          type="button"
+          onClick={handleDelete}
+          className="absolute top-2 left-2 z-10 cursor-pointer size-12 bg-background hover:bg-subtle border border-border flex items-center justify-center rounded-full transition-colors"
+        >
+          <span className="sr-only">Delete image</span>
+          <XMarkIcon className="size-5 text-soft" />
+        </button>
+      )}
+
+      <Image
+        src={src}
+        alt=""
+        fill={true}
+        sizes="(max-width: 770px) 100vw, 446px"
+        className="absolute inset-0 object-cover"
+      />
+    </button>
+  ) : (
+    <div className="w-full aspect-square relative border border-border rounded-xl overflow-hidden transition-all">
       {handleDelete && (
         <button
           type="button"
