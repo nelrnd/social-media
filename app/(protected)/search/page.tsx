fe6c1a2a2b89ac44ef98446aa45aea7f93771aa7
Nel <nel.renaudin@gmail.com>
2025-03-30 17:19:45 +0200

    Create Search component

--- app/(protected)/search/page.tsx
+++ app/(protected)/search/page.tsx
@@ -1,9 +1,13 @@
 import PageHeader from "@/app/ui/page-header"
+import Search from "@/app/ui/search"
 
 export default function SearchPage() {
   return (
     <main>
       <PageHeader title="Search" />
+      <section className="p-6">
+        <Search placeholder="Search profiles" />
+      </section>
     </main>
   )
 }
---
+++ app/ui/search.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set("query", term)
+    } else {
+      params.delete("query")
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  return (
+    <div className="relative">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        className="peer block w-full p-3 pl-10 border rounded-sm bg-background border-border outline-2 placeholder:text-soft"
+        placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 size-6 h-[18px] w-[18px] -translate-y-1/2 text-soft peer-focus:text-foreground pointer-events-none" />
+    </div>
+  )
+}
